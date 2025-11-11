e244690f41eba0aa743884da6f4a44230cf34951
Jeff Verkoeyen <jeff@clutch.engineering>
2025-11-11 12:53:48 -0500

    Fix paths.

--- ScoutWeb/Sources/ScoutWeb/Components/Scout.swift
+++ ScoutWeb/Sources/ScoutWeb/Components/Scout.swift
@@ -6,13 +6,13 @@ struct Scout: View {
   var body: some View {
     Div {
       Div {
-        ScoutImage(URL(string: "gfx/scout/Layer-1-A-Shadow.png"))
-        ScoutImage(URL(string: "gfx/scout/Layer-2-B-ScoutBody.png"))  .className("motion-safe:animate-bounce-body")
-        ScoutImage(URL(string: "gfx/scout/Layer-3-C-Tires.png"))      .className("motion-safe:animate-bounce-wheels")
-        ScoutImage(URL(string: "gfx/scout/Layer-4-D-Bike.png"))       .className("motion-safe:animate-bounce-car")
-        ScoutImage(URL(string: "gfx/scout/Layer-5-B-Upperbody.png"))  .className("motion-safe:animate-bounce-body")
-        ScoutImage(URL(string: "gfx/scout/Layer-6-E-Tongue.png"))     .className("motion-safe:animate-bounce-tongue")
-        ScoutImage(URL(string: "gfx/scout/Layer-7-B-Nose.png"))       .className("motion-safe:animate-bounce-body")
+        ScoutImage(URL(string: "/gfx/scout/Layer-1-A-Shadow.png"))
+        ScoutImage(URL(string: "/gfx/scout/Layer-2-B-ScoutBody.png"))  .className("motion-safe:animate-bounce-body")
+        ScoutImage(URL(string: "/gfx/scout/Layer-3-C-Tires.png"))      .className("motion-safe:animate-bounce-wheels")
+        ScoutImage(URL(string: "/gfx/scout/Layer-4-D-Bike.png"))       .className("motion-safe:animate-bounce-car")
+        ScoutImage(URL(string: "/gfx/scout/Layer-5-B-Upperbody.png"))  .className("motion-safe:animate-bounce-body")
+        ScoutImage(URL(string: "/gfx/scout/Layer-6-E-Tongue.png"))     .className("motion-safe:animate-bounce-tongue")
+        ScoutImage(URL(string: "/gfx/scout/Layer-7-B-Nose.png"))       .className("motion-safe:animate-bounce-body")
       }
     }
     .position(.relative)
